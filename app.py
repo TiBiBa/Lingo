3327df063c0c9988d18429285f7424461fec9da5
Timon Bakker <timonbakker@hotmail.com>
2022-01-03 17:46:20 +0100

    Created play grid

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route('/setup', methods=['GET'])
 def setup_game():
-  return render_template("setup_game.html")
+    return render_template("setup_game.html")
 
 
 @app.route('/setup', methods=['POST'])
@@ -19,14 +19,13 @@ def setup_preferences():
     if 'team1' not in body or 'team2' not in body:
         return 'Er mist een teamnaam', 400
     if body['team1'] == body['team2']:
-        return 'Teamnamen mogen niet hetzelfde zijn', 400
+        return 'Namen mogen niet hetzelfde zijn', 400
 
     return jsonify({}, 200)
 
 
 @app.route('/play')
 def play_game():
-    print("We gaan spelen!")
     return render_template("play.html")
 
 
